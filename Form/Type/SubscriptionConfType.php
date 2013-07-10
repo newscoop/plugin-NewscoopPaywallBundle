add7becbe7caf93db5c6440616009cae80e29b12
Rafał Muszyński <rmuszynski1@gmail.com>
2013-07-10 14:12:57 +0200

    some fixes

--- Form/Type/SubscriptionConfType.php
+++ Form/Type/SubscriptionConfType.php
@@ -5,38 +5,45 @@ namespace Newscoop\PaywallBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class SubscriptionConfType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', null, array(
-                    'label' => 'Name of subscription: ',
-                    'error_bubbling' => true,
-                    'invalid_message' => 'Subscription name can not be empty.'
+        ->add('name', null, array(
+            'label' => 'Name of subscription: ',
+            'attr' => array('class'=>'input_text', 'style'=>'width:300px'),
+            'error_bubbling' => true,
+            'invalid_message' => 'Subscription name can not be empty.'
             ))
-            ->add('type', 'entity', array(
-                'class' => 'Newscoop\Entity\Section',
-                'label'  => 'Type of subscription: ',
-                'property' => 'name',
+        ->add('type', 'entity', array(
+            'class' => 'Newscoop\Entity\Section',
+            'label'  => 'Type of subscription: ',
+            'property' => 'name',
+            'query_builder' => function(EntityRepository $er) {
+                return $er->createQueryBuilder('s')
+                    ->groupBy('s.name')
+                    ->orderBy('s.name', 'ASC');
+                }
             ))
-            ->add('range', null, array(
-                    'label' => 'Duration of subscription in days: ',
-                    'error_bubbling' => true,
-                    'invalid_message' => 'Type duration of subscription in days.'
+        ->add('range', null, array(
+            'label' => 'Duration of subscription in days: ',
+            'error_bubbling' => true,
+            'invalid_message' => 'Type duration of subscription in days.'
             ))
-            ->add('price', null, array(
-                    'label' => 'Value of the subscription: ',
-                    'error_bubbling' => true,
-                    'required' => true,
-                    'invalid_message' => 'Type the value of the subscription.'
+        ->add('price', null, array(
+            'label' => 'Value of the subscription: ',
+            'error_bubbling' => true,
+            'required' => true,
+            'invalid_message' => 'Type the value of the subscription.'
             ))
-            ->add('currency', null, array(
-                    'label' => 'Currency: ',
-                    'error_bubbling' => true,
-                    'required' => true,
-                    'invalid_message' => 'Type currency.'
+        ->add('currency', null, array(
+            'label' => 'Currency: ',
+            'error_bubbling' => true,
+            'required' => true,
+            'invalid_message' => 'Type currency.'
             )) 
         ;
     }
@@ -45,7 +52,7 @@ class SubscriptionConfType extends AbstractType
     {
         $resolver->setDefaults(array(
             'data_class' => 'Newscoop\PaywallBundle\Entity\Subscriptions'
-        ));
+            ));
     }
 
     public function getName()
