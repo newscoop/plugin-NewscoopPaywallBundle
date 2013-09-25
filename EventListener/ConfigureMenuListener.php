d9b44f748f91e4408f5d57e95c39ce5cdb9bef22
Rafał Muszyński <rmuszynski1@gmail.com>
2013-09-25 10:24:08 +0200

    translation fix in menu

--- EventListener/ConfigureMenuListener.php
+++ EventListener/ConfigureMenuListener.php
@@ -9,37 +9,50 @@
 namespace Newscoop\PaywallBundle\EventListener;
 
 use Newscoop\NewscoopBundle\Event\ConfigureMenuEvent;
+use Symfony\Component\Translation\Translator;
 
 class ConfigureMenuListener
-{
+{   
+    private $translator;
+
+    /**
+     * @param Translator $translator
+     */
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * @param ConfigureMenuEvent $event
      */
     public function onMenuConfigure(ConfigureMenuEvent $event)
-    {
+    {   
         $menu = $event->getMenu();
+        $labelPlugins = $this->translator->trans('Plugins');
+        $labelPluginName = $this->translator->trans('paywall.title');
 
-        $menu[getGS('Plugins')]->addChild(
-        	getGS('Newscoop Paywall'), 
+        $menu[$labelPlugins]->addChild(
+        	$labelPluginName, 
         	array('uri' => $event->getRouter()->generate('newscoop_paywall_admin_admin'))
         );
         
-        $menu[getGS('Plugins')][getGS('Newscoop Paywall')]->addChild(
-            getGS('Manage subscriptions'), 
+        $menu[$labelPlugins][$labelPluginName]->addChild(
+            $this->translator->trans('Manage subscriptions'), 
             array('uri' => $event->getRouter()->generate('newscoop_paywall_managesubscriptions_manage')
         ));
-        $menu[getGS('Plugins')][getGS('Newscoop Paywall')][getGS('Manage subscriptions')]->setDisplay(false);
+        $menu[$labelPlugins][$labelPluginName][$this->translator->trans('Manage subscriptions')]->setDisplay(false);
 
-        $menu[getGS('Plugins')][getGS('Newscoop Paywall')]->addChild(
-            getGS('Manage User Subscriptions'), 
+        $menu[$labelPlugins][$labelPluginName]->addChild(
+            $this->translator->trans('Manage User Subscriptions'), 
             array('uri' => $event->getRouter()->generate('newscoop_paywall_userssubscriptions_index')
         ));
-        $menu[getGS('Plugins')][getGS('Newscoop Paywall')][getGS('Manage User Subscriptions')]->setDisplay(false);
+        $menu[$labelPlugins][$labelPluginName][$this->translator->trans('Manage User Subscriptions')]->setDisplay(false);
 
-        $menu[getGS('Plugins')][getGS('Newscoop Paywall')]->addChild(
-            getGS('Configure Paywall'), 
+        $menu[$labelPlugins][$labelPluginName]->addChild(
+            $this->translator->trans('Configure Paywall'), 
             array('uri' => $event->getRouter()->generate('newscoop_paywall_configurepaywall_index')
         ));
-        $menu[getGS('Plugins')][getGS('Newscoop Paywall')][getGS('Configure Paywall')]->setDisplay(false);
+        $menu[$labelPlugins][$labelPluginName][$this->translator->trans('Configure Paywall')]->setDisplay(false);
     }
 }
